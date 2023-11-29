dd4819997aa7ddaba89726beb61ffc82bf2a6c54
Perla Brunilda <perlabru@ge-m3.kzn.21-school.ru>
2023-11-29 14:04:48 +0300

    Innit commit

--- src/library/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-
-@admin.register(models.Track)
-class TrackAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'album', 'file_path')
-    list_display_links = ('title'),
-    list_filter = ('file_path'),
